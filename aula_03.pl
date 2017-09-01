bf34d0f2b33194d393cff3fa34a521d0651a5c79
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-09-01 11:47:47 -0300

    Busca em Largura implementado

--- aula_03.pl
+++ aula_03.pl
@@ -1,3 +1,4 @@
+%fatos
 pode_ir(a,b).
 pode_ir(a,c).
 pode_ir(a,d).
@@ -8,12 +9,15 @@ pode_ir(d,h).
 pode_ir(h,k).
 pode_ir(h,j).
 
+%BUSCA EM PROFUNDIDADE
+
+%Saber se pertence
 pertence(E, [E|_]) :- !.
 pertence(E, [_|T]) :-
   pertence(E,T).
 
 
-%Caminho comecando de k para a
+%Caminho comecando de Final para Inicial
 caminho_busca_pro_tras(EI, EF, Cam) :-
   caminho_p_t(EF, [EI], Cam).
 
@@ -25,7 +29,7 @@ caminho_busca_pro_tras(EI, EF, Cam) :-
     caminho_p_t(EF, [X, Eaux|Caminho_percorrido], Cam).
 
 
-
+%Caminho comecando de Inicial para Final
 caminho_busca_pro_frente(EI, EF, Cam) :-
   caminho_p_f(EI, [EF], Cam).
 
@@ -35,3 +39,25 @@ caminho_busca_pro_frente(EI, EF, Cam) :-
     pode_ir(X, Eaux),
       not(pertence(X, Caminho_percorrido)),
       caminho_p_f(EI, [X, Eaux|Caminho_percorrido], Cam).
+
+
+%BUSCA EM LARGURA
+
+%CONCATENAR
+concatena([],L,L).
+concatena([X|Y],L,[X|Lista]):-
+     concatena(Y,L,Lista).
+
+%LISTA ESTENDER
+estender([No|Caminho],NovosCaminhos):-
+  findall([NovoNo,No|Caminho],(s(No,NovoNo),
+  not(pertence(NovoNo,[No|Caminho]))), NovosCaminhos).
+
+buscaLargura([[No|Caminho]|_],[No|Caminho]):-
+  final(No).
+
+buscaLargura([Caminho|OutrosCaminhos],Sol):-
+  estender(Caminho,NovosCaminhos),
+  concatenar(OutrosCaminhos,NovosCaminhos,Caminhos1),
+
+buscaLargura(Caminhos1,Sol).
