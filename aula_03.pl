4c7d0a7e441f64e9a16ff1476df27e5e28d2bbf4
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-09-01 11:20:33 -0300

    busca cimpleta

--- aula_03.pl
+++ aula_03.pl
@@ -1,10 +1,24 @@
-pode_ir(a,[b, c, d]).
-pode_ir(b,[e,f]).
-pode_ir(b,[f]).
-pode_ir(c,[g]).
-pode_ir(d,[h]).
-pode_ir(h,[k,j]).
+pode_ir(a,b).
+pode_ir(a,c).
+pode_ir(a,d).
+pode_ir(b,e).
+pode_ir(b,f).
+pode_ir(c,g).
+pode_ir(d,h).
+pode_ir(h,k).
+pode_ir(h,j).
 
-busca_pro(X,Y) :-
-    pode_ir(X,Y),
-    busca_pro(Y,Z).
+pertence(E, [E|_]) :- !.
+pertence(E, [_|T]) :-
+  pertence(E,T).
+
+
+caminho_busca_pro_tras(EI, EF, Cam) :-
+  caminho_p_t(EF, [EI], Cam).
+
+  caminho_p_t(EF, [EF|Cam], [EF|Cam]).
+
+  caminho_p_t(EF, [Eaux|Caminho_percorrido],Cam) :-
+    pode_ir(Eaux, X),
+    not(pertence(X, Caminho_percorrido)),
+    caminho_p_t(EF, [X, Eaux|Caminho_percorrido], Cam).
