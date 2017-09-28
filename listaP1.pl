95065b3ad3dff5e926e518d87c664b624a3dbc57
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-09-27 22:29:29 -0300

    ëxercicio 1 feito

--- listaP1.pl
+++ listaP1.pl
@@ -1,5 +1,31 @@
+/*
+    Exercicio 1 da lista de prolog
+    Corrigido os erros, anotando em comentarios
+*/
+
 bruxo(ron).
 possuiVarinha(harry).
 jogadorQuadribol(harry).
+/* temVarinha, diferente de possui varinha*/
 bruxo(X) :- possuiVassoura(X), possuiVarinha(X).
+/* temVassoura, diferente de possui vassoura*/
 possuiVassoura(X) :- jogadorQuadribol(X).
+
+/*
+  Item b:
+
+  True para:
+  bruxo(ron).
+  bruxo(harry).
+
+  false para:
+  bruxo(hermione).
+
+  correcao para sintaxe correta:
+  bruxa(ron).
+  bruxa(hermione).
+  bruxa(Y).
+
+  bruxo(Y), respostas:
+  ron e harry
+*/
